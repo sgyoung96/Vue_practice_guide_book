79c499ccafd7f6931364acfbd8ef7e68d11351d5
sgyoung96 <sgyoung96@naver.com>
2023-05-16 20:41:24 +0900

    create code snnipet

--- Vue_practice/vue-project-manager/src/views/AboutView.vue
+++ Vue_practice/vue-project-manager/src/views/AboutView.vue
@@ -1,5 +1,20 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<template></teemplate>
+<script>/nexport default {
+  name: '',
+  components: {}. 
+  data() {
+    return {
+      sampleData: ''
+    };
+  },
+  beforeCreate() {}, 
+  created() {}, 
+  beforeMount() {},
+  mounted() {}, 
+  beforeUpdate() {}, 
+  updated() {}, 
+  beforeUnmount(){}, 
+  unmounted() {}, 
+  methods: {} 
+}
+</script>
