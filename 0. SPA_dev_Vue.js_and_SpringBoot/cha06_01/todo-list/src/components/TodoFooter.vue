ead45e0d99346219d656409eae3a6704cbf3e8ec
sgyoung96 <sgyoung96@naver.com>
2023-05-29 20:58:19 +0900

    todo list 컴포넌트 정의

---
+++ 0. SPA_dev_Vue.js_and_SpringBoot/cha06_01/todo-list/src/components/TodoFooter.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>TodoFooter</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+    div {
+        background-color: #00FFFF;
+    }
+
+</style>
---
+++ 0. SPA_dev_Vue.js_and_SpringBoot/cha06_01/todo-list/src/components/TodoHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>TodoHeader</h1>
+    </div>
+</template>
+
+<script>
+
+    export default {
+
+    }
+
+</script>
+
+<style scoped>
+
+    div {
+        background-color: #00FF00;
+    }
+
+</style>
